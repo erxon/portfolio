4215cd9b9cf8dfae59fb02c32e9436c85549c490
Ericson Castasus <castasuse@gmail.com>
2025-08-04 14:26:42 +0800

    add useeffect in threads

--- src/app/components/Threads.tsx
+++ src/app/components/Threads.tsx
@@ -2,12 +2,19 @@
 
 import { useTheme } from "next-themes";
 import ThreadsBase from "./ThreadsBase";
+import { useEffect, useState } from "react";
 
 export default function Threads() {
+  const [currentTheme, setCurrentTheme] = useState<string>("");
   const { theme } = useTheme();
+
+  useEffect(() => {
+    setCurrentTheme(theme!);
+  }, [theme]);
+
   return (
     <ThreadsBase
-      color={theme === "light" ? [0, 0, 0] : [1, 1, 1]}
+      color={currentTheme === "light" ? [0, 0, 0] : [1, 1, 1]}
       amplitude={1}
       distance={0.1}
       enableMouseInteraction={true}
