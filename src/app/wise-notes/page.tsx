dc70425345c3489174ffbe18df08ee06dfcf0341
Ericson Castasus <castasuse@gmail.com>
2025-10-31 14:47:10 +0800

    fix view

--- src/app/wise-notes/page.tsx
+++ src/app/wise-notes/page.tsx
@@ -14,46 +14,48 @@ import { ModeToggle } from "@/components/ui/theme-mode-toggle";
 export default function Page() {
   return (
     <>
-      <div className="px-[5%] lg:px-[18%] pt-12">
-        <div className="flex justify-between items-center mx-auto bg-white dark:bg-gray-900 p-4 rounded-lg shadow-lg">
-          <Link
-            className={
-              "transition hover:border-b-1 hover:border-b-black flex gap-2 w-fit h-7"
-            }
-            href={"/"}
-          >
-            <ArrowLeft /> Ericson&apos;s Portfolio
-          </Link>
-          <ModeToggle />
-        </div>
-        <div className="flex flex-col gap-8 text-center items-center justify-center h-[calc(100vh-175px)] mb-10">
-          <p className="font-bold text-4xl">WISE NOTES</p>
-          <p className="md:w-[75%] text-md md:text-lg text-balance tracking-wide">
-            Wise notes is a web based note application that has RAG
-            (Retrieval-Augmented Generation) integration. It is made with
-            <span className="font-bold"> Typescript React</span> in front-end
-            and <span className="font-bold">NodeJS, Express,</span> and{" "}
-            <span className="font-bold">MongoDB</span> in the back-end. It has a
-            feature where the user can query the integrated model about their
-            notes. It can synthesize and summarize notes according to
-            users&apos; needs. This application implements this basic idea, as
-            it expands and improve, this system can be used as a research tool,
-            a knowledge base for organizations, and as a database for expert
-            systems.
-          </p>
-          <div className="flex gap-2 mt-8">
-            <Button asChild>
-              <Link href={"https://wise-notes-v2-8ppi.vercel.app/"}>
-                Live demo <SquareArrowOutUpRight />
-              </Link>
-            </Button>
+      <div className="px-[5%] lg:px-[18%] pt-8">
+        <div className="h-screen">
+          <div className="flex justify-between items-center mx-auto bg-white dark:bg-gray-900 p-4 rounded-lg shadow-lg box-border">
+            <Link
+              className={
+                "transition hover:border-b-1 hover:border-b-black flex gap-2 w-fit"
+              }
+              href={"/"}
+            >
+              <ArrowLeft /> Ericson&apos;s Portfolio
+            </Link>
+            <ModeToggle />
+          </div>
+          <div className="flex flex-col gap-8 text-center items-center justify-center h-[calc(100vh-100px)]">
+            <p className="font-bold text-4xl">WISE NOTES</p>
+            <p className="md:w-[75%] text-md md:text-lg text-balance tracking-wide">
+              Wise notes is a web based note application that has RAG
+              (Retrieval-Augmented Generation) integration. It is made with
+              <span className="font-bold"> Typescript React</span> in front-end
+              and <span className="font-bold">NodeJS, Express,</span> and{" "}
+              <span className="font-bold">MongoDB</span> in the back-end. It has
+              a feature where the user can query the integrated model about
+              their notes. It can synthesize and summarize notes according to
+              users&apos; needs. This application implements this basic idea, as
+              it expands and improve, this system can be used as a research
+              tool, a knowledge base for organizations, and as a database for
+              expert systems.
+            </p>
+            <div className="flex gap-2 mt-8">
+              <Button asChild>
+                <Link href={"https://wise-notes-v2-8ppi.vercel.app/"}>
+                  Live demo <SquareArrowOutUpRight />
+                </Link>
+              </Button>
 
-            <Button variant={"outline"} asChild>
-              <Link href={"https://github.com/erxon/wise-notes-v2"}>
-                {" "}
-                GitHub <GitHubIcon reverse />
-              </Link>
-            </Button>
+              <Button variant={"outline"} asChild>
+                <Link href={"https://github.com/erxon/wise-notes-v2"}>
+                  {" "}
+                  GitHub <GitHubIcon reverse />
+                </Link>
+              </Button>
+            </div>
           </div>
         </div>
         <Features />
